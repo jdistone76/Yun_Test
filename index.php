6d2e46548128573848b23fe456353a32af86eb55
jdistone76 <60934064+jdistone76@users.noreply.github.com>
2020-02-12 01:01:37 +0900

    First Version

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!doctype html>
+<html>
+    
+    <head>
+        <title> Yun-1 Test</title>
+        <meta charset="UTF-8">
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158187643-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-158187643-1');
+        </script>
+
+    </head>
+    
+    <body>    
+        <h1> YUN First Home</h1>
+        <p>
+            <iframe width="560" 
+                   height="315" 
+                   src="https://www.youtube.com/embed/alzrz4jFsrI" 
+                   frameborder="0" 
+                   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                   allowfullscreen>                       
+            </iframe>
+        </p>
+        
+        <p>
+            
+            
+        </p>
+        <ol>
+            <li>1.HTML</li>
+            <li>2.CSS</li>
+            <li>3.Java Script</li>
+            <li>youbin</li>
+         </ol>
+
+         <ul>
+            <li>1.Tekla</li>
+            <li>2.Yun</li>
+            <li>3.YouBin YeoJun</li>
+         </ul>
+
+         <ol>
+            <li>Tekla</li>
+            <li>Yun</li>
+            <li>YouBin YeoJun</li>
+         </ol>
+
+        <h1> <a href="http://www.w3.org/TR/html401/" target="_blank" title="html 설명이여">혹시 한글도?</a></h1>
+            <p>In need of some extra <strong>support</strong> in your running shoes? </p>
+            <p>Look no further than the DoctorInsole FitStep orthotic insoles, <br>designed for active men and women.</p>
+            <p>Created by podiatrist Robert Joseph, these insoles aim to relieve a number of issues common in runners, </p>
+            <p>including plantar fasciitis, <u>shin splints</u>, ankle weakness and more.</p>
+
+            <img src="image.png" width="50%" >
+            
+            
+            <p>
+                <div id="disqus_thread"></div>
+                <script>
+
+                /**
+                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                /*
+                var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+                */
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://yuntest.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                      
+            </p>
+            <!--Start of Tawk.to Script-->
+                <script type="text/javascript">
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/5e42c74da89cda5a18855a92/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+                </script>
+                <!--End of Tawk.to Script-->
+            
+            <p>
+                
+            <p>
+                
+            </p>
+     </body> 
+
+</html>
